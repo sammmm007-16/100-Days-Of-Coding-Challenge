462759671e39411ebebaed5de97a1dc5d312200b
sammmm007-16 <sameer.38094@stu.upes.ac.in>
2026-08-19 00:47:10 +0530

    Add Question 02 in Day 01

---
+++ Day01/Que02.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+	int a, b;
+	printf("Enter Two Integers : ");
+	scanf("%d %d", &a, &b);
+	printf("Sum = %d\n",a + b);
+	printf("Difference = %d\n",a - b);
+	printf("Product = %d\n",a * b);
+	printf("Quotient = %d\n",a / b);
+	
+	return 0;
+}
