8e2fb7f5647a011ef1a0a1fc6c05a02be4026aa8
sammmm007-16 <sameer.38094@stu.upes.ac.in>
2026-09-26 12:28:07 +0530

    Add files via upload

---
+++ Day28/Que02.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i, a[100];
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    for (i = 0; i < n; i++)
+    {
+        printf("%d ", a[i]);
+    }
+
+    return 0;
+}
