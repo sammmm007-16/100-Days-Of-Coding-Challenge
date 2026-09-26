ffdb0028c7991fa3b85da64b17e0afbb7b6b85c1
sammmm007-16 <sameer.38094@stu.upes.ac.in>
2026-09-26 12:56:51 +0530

    Add files via upload

---
+++ Day45/Que01.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    char str[100], ch;
+    int i = 0, count = 0;
+
+    printf("Enter a string: ");
+    fgets(str, 100, stdin);
+
+    printf("Enter character: ");
+    scanf("%c", &ch);
+
+    while (str[i] != '\0')
+    {
+        if (str[i] == ch)
+        {
+            count++;
+        }
+
+        i++;
+    }
+
+    printf("%d", count);
+
+    return 0;
+}
