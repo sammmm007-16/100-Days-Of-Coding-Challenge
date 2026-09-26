8752467a21c806745b276003a1196878463aa4d0
sammmm007-16 <sameer.38094@stu.upes.ac.in>
2026-09-26 12:29:20 +0530

    Add files via upload

---
+++ Day31/Que01.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i, a[100], key, index = -1;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    printf("Enter element to search: ");
+    scanf("%d", &key);
+
+    for (i = 0; i < n; i++)
+    {
+        if (a[i] == key)
+        {
+            index = i;
+            break;
+        }
+    }
+
+    if (index != -1)
+    {
+        printf("Found at index %d", index);
+    }
+    else
+    {
+        printf("-1");
+    }
+
+    return 0;
+}
---
+++ Day31/Que02.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i, temp, a[100];
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    for (i = 0; i < n / 2; i++)
+    {
+        temp = a[i];
+        a[i] = a[n - 1 - i];
+        a[n - 1 - i] = temp;
+    }
+
+    for (i = 0; i < n; i++)
+    {
+        printf("%d ", a[i]);
+    }
+
+    return 0;
+}
