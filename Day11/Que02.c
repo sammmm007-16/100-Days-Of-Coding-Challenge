01f0cc0f45f686cda8046289519fd2e151325498
sammmm007-16 <sameer.38094@stu.upes.ac.in>
2026-09-26 11:23:30 +0530

    Add files via upload

---
+++ Day11/Que02.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main()
+{
+    float cp, sp, profit, loss, percentage;
+
+    printf("Enter cost price: ");
+    scanf("%f", &cp);
+
+    printf("Enter selling price: ");
+    scanf("%f", &sp);
+
+    if (sp > cp)
+    {
+        profit = sp - cp;
+        percentage = (profit / cp) * 100;
+
+        printf("Profit = %f\n", profit);
+        printf("Profit Percentage = %f", percentage);
+    }
+    else if (cp > sp)
+    {
+        loss = cp - sp;
+        percentage = (loss / cp) * 100;
+
+        printf("Loss = %f\n", loss);
+        printf("Loss Percentage = %f", percentage);
+    }
+    else
+    {
+        printf("No Profit No Loss");
+    }
+
+    return 0;
+}
