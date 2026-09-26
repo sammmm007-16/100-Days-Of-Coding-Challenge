d06ed1a45d3d18f5563962bbcba4cc8c6b0b0f7b
sammmm007-16 <sameer.38094@stu.upes.ac.in>
2026-09-26 12:27:37 +0530

    Add files via upload

---
+++ Day26/Que02.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+    int i;
+
+    for (i = 1; i <= 4; i++)
+    {
+        printf("*\n");
+    }
+
+    printf("\n");
+
+    for (i = 1; i <= 5; i++)
+    {
+        printf("*\n");
+    }
+
+    printf("\n");
+
+    for (i = 1; i <= 3; i++)
+    {
+        printf("*\n");
+    }
+
+    printf("\n");
+
+    printf("*");
+
+    return 0;
+}
