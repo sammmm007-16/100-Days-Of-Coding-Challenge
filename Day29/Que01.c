8ec0387a89eb259e93c145b4010829fd9897fdbd
sammmm007-16 <sameer.38094@stu.upes.ac.in>
2026-09-26 12:28:22 +0530

    Add files via upload

---
+++ Day29/Que01.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i, a[100], sum = 0;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+        sum = sum + a[i];
+    }
+
+    printf("Sum = %d", sum);
+
+    return 0;
+}
